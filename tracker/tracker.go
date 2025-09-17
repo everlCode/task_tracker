a33b1bb493fae00d393ba28d6f65ad2f4a134b06
everl <kolukul1996@gmail.com>
2025-09-17 22:22:08 +0300

    tarcker main methods

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -13,3 +13,23 @@ func (tracker Tracker) Add(name string) {
 	fmt.Println("ADD METHOD EXECUTE")
 	fmt.Println(name)
 }
+
+func (tracker Tracker) Update(id int, name string) {
+	fmt.Println("Update METHOD EXECUTE")
+}
+
+func (tracker Tracker) Delete(id int) {
+	fmt.Println("ADeleteDD METHOD EXECUTE")
+}
+
+func (tracker Tracker) MarkInProgress(id int) {
+	fmt.Println("mark-in-progress METHOD EXECUTE")
+}
+
+func (tracker Tracker) MarkDone(id int) {
+	fmt.Println("MarkDone METHOD EXECUTE")
+}
+
+func (tracker Tracker) List(id int) {
+	fmt.Println("List METHOD EXECUTE")
+}
